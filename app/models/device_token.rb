8d93f422e8f9d22a3a7d919e58bdec86e0c67658
Vadim Stroganov <vadimstroganov@gmail.com>
2017-05-15 00:27:01 +0300

    fix typo

--- app/models/device_token.rb
+++ app/models/device_token.rb
@@ -3,10 +3,13 @@ class DeviceToken < ApplicationRecord
   validates :device_token_type_id, presence: true
   validates :value, presence: true
 
-  def self.build_token(user_id, value, type)
-    device_type_type = DeviceTokenType.find_by(name: type)
-    return nil unless device_type_type.present?
+  belongs_to :user
+  belongs_to :device_token_type
 
-    DeviceToken.find_or_create_by(user_id: user_id, value: value, type: device_type_type.id)
+  def self.build_token(user_id, value, type)
+    device_token_type = DeviceTokenType.find_by(name: type)
+    return nil unless device_token_type.present?
+
+    DeviceToken.find_or_create_by(user_id: user_id, value: value, device_token_type_id: device_token_type.id)
   end
 end
--- app/models/device_token_type.rb
+++ app/models/device_token_type.rb
@@ -1,3 +1,5 @@
 class DeviceTokenType < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  has_many :device_tokens, dependent: :destroy
 end
